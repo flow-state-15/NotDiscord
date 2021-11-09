d4fbb7bf7cdd4df28ce0c3dd91e363a64811080b
Concrete18 <michaelericson19@gmail.com>
2021-11-09 17:01:59 -0500

    finished server routes except for delete testing due to needed delete cascade

--- app/api/servers.py
+++ app/api/servers.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from flask_login import login_required
-from app.models import Server, User_Server, User
+from app.models import db, Server, User_Server, User
+from werkzeug.security import generate_password_hash
+from random import randint
 
 
 server_routes = Blueprint("servers", __name__)
@@ -12,7 +14,6 @@ server_routes = Blueprint("servers", __name__)
 @login_required
 def get_all_servers():
     servers = Server.query.all()
-    print(servers)
     return {"servers": [server.to_dict() for server in servers]}
 
 
@@ -47,18 +48,40 @@ def logged_in_start(user_id):
 @server_routes.route('/', methods=['POST'])
 @login_required
 def post_all_servers():
-    return "<h1>create server</h1>"
+    data = request.json
+    if 'icon' in data.keys():
+        icon = data["icon"]
+    else:
+        icon = ''
+    server = Server(
+        name=data["name"],
+        owner_id=data["owner_id"],
+        icon=icon,
+        invite_link=generate_password_hash(f'{data["name"]}{randint(1, 21)}')[-8:]
+    )
+    db.session.add(server)
+    db.session.commit()
+    return server.to_dict()
 
 
-# PUT create server
-@server_routes.route('/<int:server_id>', methods=['PUT'])
+# PUT update server
+@server_routes.route('update/<int:server_id>', methods=['PUT'])
 @login_required
 def update_server(server_id):
-    return "<h1>update server</h1>"
+    server = Server.query.get(server_id)
+    data = request.json
+    if 'icon' in data.keys():
+        server.icon = data["icon"]
+    if 'name' in data.keys():
+        server.name = data["name"]
+    db.session.commit()
+    return server.to_dict()
 
 
 # DELETE server
-@server_routes.route('/<int:server_id>', methods=['DELETE'])
+@server_routes.route('delete/<int:server_id>', methods=['DELETE'])
 @login_required
 def delete_server(server_id):
-    return "<h1>delete server</h1>"
+    db.session.query(Server).filter(Server.id==server_id).delete()
+    db.session.commit()
+    return {'server_id': server_id}
