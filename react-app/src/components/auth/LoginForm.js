3b2ea4c86860c738e6ba6962421c4f2e075cbd46
Nebyou <nebyou.4k@gmail.com>
2021-11-09 18:27:09 -0500

    Fixes

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
-import { login } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, Link } from "react-router-dom";
+import { login } from "../../store/session";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onLogin = async (e) => {
@@ -27,45 +27,10 @@ const LoginForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/channels/@me' />;
+    return <Redirect to="/channels/@me" />;
   }
 
   return (
-<<<<<<< HEAD
-    <form className="login-form" onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button
-          type='submit'
-          onClick={() => console.log("LOGIN CLICKED")}
-        >
-          Login
-        </button>
-      </div>
-    </form>
-=======
     <div>
       <form onSubmit={onLogin}>
         <div>
@@ -74,35 +39,31 @@ const LoginForm = () => {
           ))}
         </div>
         <div>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor="email">Email</label>
           <input
-            name='email'
-            type='text'
-            placeholder='Email'
+            name="email"
+            type="text"
+            placeholder="Email"
             value={email}
             onChange={updateEmail}
           />
         </div>
         <div>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <input
-            name='password'
-            type='password'
-            placeholder='Password'
+            name="password"
+            type="password"
+            placeholder="Password"
             value={password}
             onChange={updatePassword}
           />
-          <button
-            type='submit'
-            onClick={() => console.log("LOGIN CLICKED")}
-          >
+          <button type="submit" onClick={() => console.log("LOGIN CLICKED")}>
             Login
           </button>
         </div>
       </form>
       <Link to="/">Return to Discord</Link>
     </div>
->>>>>>> master
   );
 };
 
