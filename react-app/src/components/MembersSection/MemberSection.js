55043c32acbe63b0533d83ffca25dfad6ef2accd
flow-state-15 <purcellgroup88@gmail.com>
2021-11-09 10:48:44 -0600

    merged jasons work

--- react-app/src/components/MembersSection/MemberSection.js
+++ react-app/src/components/MembersSection/MemberSection.js
@@ -1,9 +1,6 @@
 export default function MemberSection({ member }) {
-<<<<<<< HEAD
-=======
     // console.log("**** IN MEMBER SECTION, member: ", member)
     const memberUsername = member.tagged_name.split('#')[0];
->>>>>>> master
     return (
         <div className="member-section">
             <h2>{memberUsername}</h2>
