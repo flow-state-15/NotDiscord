321d973c91a626e08b2d1e0d8ab5b4f76adca0ba
Concrete18 <michaelericson19@gmail.com>
2021-11-09 15:40:57 -0500

    fixed message date and time

--- react-app/src/components/MessagesSection/MessageSection/index.js
+++ react-app/src/components/MessagesSection/MessageSection/index.js
@@ -1,10 +1,16 @@
 
 export default function MessageSection({ message }) {
     const event = new Date(message.sent_date);
-    console.log('non converted date', event)
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    };
     const converted = event.toLocaleDateString(undefined, options)
-    console.log('converted date', converted)
+    
     return (
         <div className="message-section">
             <p>{ message.content }</p>
