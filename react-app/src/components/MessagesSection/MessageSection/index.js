36d2f6dae58a7ef9c19c40d24f79a24c0267ff70
Concrete18 <michaelericson19@gmail.com>
2021-11-09 14:20:13 -0500

    added time to message info

--- react-app/src/components/MessagesSection/MessageSection/index.js
+++ react-app/src/components/MessagesSection/MessageSection/index.js
@@ -2,7 +2,7 @@
 export default function MessageSection({ message }) {
     const event = new Date(message.sent_date);
     console.log('non converted date', event)
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
     const converted = event.toLocaleDateString(undefined, options)
     console.log('converted date', converted)
     return (
