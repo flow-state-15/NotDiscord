2fae03fc473946664ba1eb8cb4fb7cfbc3d67584
Concrete18 <michaelericson19@gmail.com>
2021-11-10 18:10:32 -0500

    worked on a new shortcut for git merges

--- database.bash
+++ database.bash
@@ -1,11 +1,18 @@
 if [ $1 == 'reseed' ]; then
     flask seed undo
     flask seed all
+elif [ $1 == 'hreseed' ]; then
+    flask seed undo
+    flask seed all
 elif [ $1 == 'reset' ]; then
     flask db init
     flask db migrate
     flask db upgrade
-    flask seed
+    flask seed all
+elif [ $1 == 'pullmaster' ]; then
+    env -i git checkout master
+    env -i git pull
+    env -i git checkout $2
 else
     echo "Unknown arg given. $1 is invalid."
 fi
