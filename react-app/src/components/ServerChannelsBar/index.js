bdc14f97326ee09880b6388a9863e65862395c63
Concrete18 <michaelericson19@gmail.com>
2021-11-11 17:31:22 -0500

    fixed merge

--- react-app/src/components/ServerChannelsBar/index.js
+++ react-app/src/components/ServerChannelsBar/index.js
@@ -14,45 +14,6 @@ export default function ServerChannelsBar({ channels }) {
 
   return (
     <div className="server-channels-bar-inner-content">
-<<<<<<< HEAD
-      <div className="server-channels-bar-header">
-        <h3 className="server-channels-bar-header-title">{`${
-          currentServer?.name.length >= 15
-            ? currentServer?.name.slice(0, 19) + "..."
-            : currentServer?.name
-        }`}</h3>
-        <div className="mess-with-it-later">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="#D5D5D6"
-            width="18px"
-            height="18px"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </div>
-      </div>
-      <h3 className="server-channels-title">TEXT CHANNELS</h3>
-      <div className="server-channels-listings">
-        {channels.map((channel) => {
-          return (
-            <ServerChannelListing
-              key={channel.id}
-              channel={channel}
-            ></ServerChannelListing>
-          );
-        })}
-      </div>
-      <UserControls />
-=======
       {currentServer && (
         <>
           <div className="server-channels-bar-header">
@@ -95,7 +56,6 @@ export default function ServerChannelsBar({ channels }) {
           <UserControls />
         </>
       )}
->>>>>>> master
     </div>
   );
 }
