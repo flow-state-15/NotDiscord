0ab6b150a84644c5bcfeb0d8e9728a8e9bc8458b
flow-state-15 <purcellgroup88@gmail.com>
2021-11-11 16:57:16 -0600

    progress on home

--- react-app/src/components/DiscordHome/index.js
+++ react-app/src/components/DiscordHome/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 import { login } from "../../store/session";
+import './DiscordHome.css'
 
 export default function DiscordHome() {
   const [demo, setDemo] = useState(false);
@@ -17,6 +18,15 @@ export default function DiscordHome() {
   return (
     <div>
       <h1>Discord Splash Page</h1>
+      <div className='home-banner-wrapper'>
+        <h1>IMGINE A PLACE...</h1>
+        <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+      </div>
+      <div className='home-background-wrapper'>
+        <img id='img-home-right' src='https://res.cloudinary.com/dan-purcell-2021/image/upload/v1636669260/discord_group_projo_assets/home-right_gxykcx.svg' alt='discord home bottom left'/>
+        <img id='img-home-center' src='https://res.cloudinary.com/dan-purcell-2021/image/upload/v1636669376/discord_group_projo_assets/home-center_jgb22n.svg' alt='discord home center'/>
+        <img id='img-home-left' src='https://res.cloudinary.com/dan-purcell-2021/image/upload/v1636669251/discord_group_projo_assets/home-left_r87yul.svg' alt='discord home bottom right'/>
+      </div>
       <button onClick={dispatchDemoLogin}>Demo User</button>
       {demo && user ? <Redirect to="/channels/@me" /> : null}
       {user ? (
