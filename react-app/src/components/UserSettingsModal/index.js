9e88e3a205b72634268f8129090ad7ac648999c0
flow-state-15 <purcellgroup88@gmail.com>
2021-11-14 14:34:06 -0600

    completed gear css

--- react-app/src/components/UserSettingsModal/index.js
+++ react-app/src/components/UserSettingsModal/index.js
@@ -47,36 +47,43 @@ export default function UserSettingsModal() {
       </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <div className="edit-user-modal">
-            <form className="edit-user-form" onSubmit={editUser}>
-              <label htmlFor="edit-user-name">User Name</label>
-              <input
-                id="edit-user-name"
-                className="edit-user-name"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              ></input>
-              <label htmlFor="edit-user-hash">User Hash</label>
-              <input
-                id="edit-user-hash"
-                className="edit-user-hash"
-                value={userHash}
-                onChange={(e) => setUserHash(e.target.value)}
-              ></input>
-              <label htmlFor="edit-user-avatar">User Avatar</label>
-              <input
-                id="edit-user-avatar"
-                className="edit-user-avatar"
-                value={userAvatar}
-                onChange={(e) => setUserAvatar(e.target.value)}
-              ></input>
+          <div className="add-server-modal">
+            <form className="add-server-form" onSubmit={editUser}>
+              <h1>Edit your user settings</h1>
+              <div className="add-server-form-inputs">
+                <label htmlFor="edit-user-name">User Name</label>
+                <input
+                  id="edit-user-name"
+                  className="add-server-name"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                ></input>
+                <label htmlFor="edit-user-hash">User Hash</label>
+                <input
+                  id="edit-user-hash"
+                  className="edit-user-hash"
+                  value={userHash}
+                  onChange={(e) => setUserHash(e.target.value)}
+                ></input>
+                <label htmlFor="edit-user-avatar">User Avatar</label>
+                <input
+                  id="edit-user-avatar"
+                  className="edit-user-avatar"
+                  value={userAvatar}
+                  onChange={(e) => setUserAvatar(e.target.value)}
+                ></input>
+              </div>
               <button className="submit-edit-user-modal" type="submit">
-                Edit User Info
+                Submit Changes
               </button>
+              <div className="add-server-modal-button-container">
+                <button className='back-add-server-modal'>Back</button>
+                <button className='user-settings-delete' onClick={removeUser}>
+                  Delete Account
+                </button>
+                <button className='user-settings-logout'>Logout</button>
+              </div>
             </form>
-            <button className="delete-account" onClick={removeUser}>
-              Delete Account
-            </button>
           </div>
         </Modal>
       )}
